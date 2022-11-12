686f1821e7e83ce1f3b228a719fd6662648d4316
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-13 03:05:02 +0600

    navbar responsiveness issue solved

--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -5,26 +5,29 @@ import { LinkContainer } from 'react-router-bootstrap';
 const NavBar = () => {
     return (
         <>
-            <Navbar>
+            <Navbar expand="lg">
                 <Container>
                     <Navbar.Brand to="/">Doctors Portal</Navbar.Brand>
-                    <Nav className="text-end">
-                        <LinkContainer to="/">
-                            <Nav.Link>Home</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/appointment">
-                            <Nav.Link>Appointment</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/about">
-                            <Nav.Link>About</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/reviews">
-                            <Nav.Link>Reviews</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <LinkContainer to="/">
+                                <Nav.Link>Home</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/appointment">
+                                <Nav.Link>Appointment</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/about">
+                                <Nav.Link>About</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/reviews">
+                                <Nav.Link>Reviews</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>Login</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
 
