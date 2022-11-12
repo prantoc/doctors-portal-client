ef76890ea919a2d25cf532d4e8f4f81e77aed570
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-12 22:48:01 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Routes/Router/Router';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
