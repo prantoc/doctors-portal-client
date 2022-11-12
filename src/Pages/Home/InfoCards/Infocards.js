e0f4cedd02e9f337a3b374cc17d585dec2ba6844
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-13 00:55:34 +0600

    dynamically info card details show

--- src/Pages/Home/InfoCards/Infocards.js
+++ src/Pages/Home/InfoCards/Infocards.js
@@ -4,61 +4,52 @@ import clock from '../../../assets/imgs/icons/clock.svg'
 import location from '../../../assets/imgs/icons/marker.svg'
 import phone from '../../../assets/imgs/icons/phone.svg'
 const Infocards = () => {
+    const cards = [
+        {
+            id: 1,
+            img: clock,
+            name: 'Opening Hours',
+            desc: 'Lorem Ipsum is simply dummy text of the pri',
+            class: 'clock-card',
+        },
+        {
+            id: 2,
+            img: location,
+            name: 'Visit our location',
+            desc: 'Brooklyn, NY 10036, United States',
+            class: 'location-card',
+        },
+        {
+            id: 3,
+            img: phone,
+            name: 'Opening Hours',
+            desc: '+000 123 456789',
+            class: 'clock-card',
+        },
+    ]
     return (
         <>
             <Container className='mt-5'>
                 <Row>
-                    <Col md={4} sm={12} className="mb-3">
-                        <Card className="clock-card">
-                            <Card.Body>
-                                <Row>
-                                    <Col lg={3} md={12} sm={3} className="my-auto mb-3">
-                                        <img src={clock} alt="" className='img-fluid' />
-                                    </Col>
-                                    <Col lg={9} md={12} sm={9}>
-                                        <Card.Subtitle className="mb-3">Opening Hours</Card.Subtitle>
-                                        <Card.Text>
-                                            Lorem Ipsum is simply dummy text of the pri
-                                        </Card.Text>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={4} sm={12} className="mb-3">
-                        <Card className="location-card">
-                            <Card.Body>
-                                <Row>
-                                    <Col lg={3} md={12} sm={3} className="my-auto mb-3">
-                                        <img src={location} alt="" className='img-fluid' />
-                                    </Col>
-                                    <Col lg={9} md={12} sm={9}>
-                                        <Card.Subtitle className="mb-3">Visit our location</Card.Subtitle>
-                                        <Card.Text>
-                                            Brooklyn, NY 10036, United States
-                                        </Card.Text>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={4} sm={12} className="mb-3">
-                        <Card className="clock-card">
-                            <Card.Body>
-                                <Row>
-                                    <Col lg={3} md={12} sm={3} className="my-auto mb-3">
-                                        <img src={phone} alt="" className='img-fluid' />
-                                    </Col>
-                                    <Col lg={9} md={12} sm={9}>
-                                        <Card.Subtitle className="mb-3">Contact us now</Card.Subtitle>
-                                        <Card.Text>
-                                            +000 123 456789
-                                        </Card.Text>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {
+                        cards.map(data => <Col key={data.id} md={4} sm={12} className="mb-3">
+                            <Card className={data.class}>
+                                <Card.Body>
+                                    <Row>
+                                        <Col lg={3} md={12} sm={3} className="my-auto mb-3">
+                                            <img src={data.img} alt="" className='img-fluid' />
+                                        </Col>
+                                        <Col lg={9} md={12} sm={9}>
+                                            <Card.Subtitle className="mb-3">{data.name}</Card.Subtitle>
+                                            <Card.Text>
+                                                {data.desc}
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
+                                </Card.Body>
+                            </Card>
+                        </Col>)
+                    }
                 </Row>
             </Container>
         </>
