04b3792085eba4aacc7080d794f09b6ba374eb9e
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-16 03:10:03 +0600

    loader use for loading

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -4,15 +4,20 @@ import { format } from 'date-fns';
 import AppointmentOption from './AppointmentOption';
 import BookingModal from '../BookingModal/BookingModal';
 import { useQuery } from '@tanstack/react-query';
+import Loading from '../../Shared/Loading/Loading';
 const AvailableAppointments = ({ selectedDate }) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const [treatment, setTreatment] = useState(null)
     const date = format(selectedDate, 'PP');
-    const { data: appointmentOption = [], refetch } = useQuery({
+    const { data: appointmentOption = [], refetch, isLoading } = useQuery({
         queryKey: ['appointment-options', date],
         queryFn: () => fetch(`http://localhost:5000/appointment-options?date=${date}`).then(res => res.json())
     })
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
     return (
         <>
