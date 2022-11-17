11f4f77cf95164dd5293d074756f0822d23d1e48
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-18 02:00:18 +0600

    make admin function

--- src/Pages/Dashboard/Users/Users.js
+++ src/Pages/Dashboard/Users/Users.js
@@ -1,13 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Button, Table } from 'react-bootstrap';
+import { successToast } from '../../../toast/Toaster';
 import Loading from '../../Shared/Loading/Loading';
 
 const Users = () => {
-    const { isLoading, data: users = [] } = useQuery({
+    const { isLoading, data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: () => fetch(`http://localhost:5000/users`).then(res => res.json())
     })
+
+    const handleMakeAdmin = id => {
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PUT'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    successToast('Role set successfully as an admin')
+                    refetch()
+                }
+            })
+    }
+
     if (isLoading) {
         return <Loading></Loading>
     }
@@ -32,7 +47,7 @@ const Users = () => {
                                 <td>{i + 1}</td>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td><Button variant='primary'>Make Admin</Button></td>
+                                <td>{user?.role !== 'admin' && <Button variant='primary' onClick={() => handleMakeAdmin(user._id)}>Make Admin</Button>}</td>
                                 <td><Button variant='danger'>Delete</Button></td>
                             </tr>
                         )}
