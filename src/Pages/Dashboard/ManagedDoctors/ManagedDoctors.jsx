ef53637ed8ad52a9bda5ec3d827132e6ea7595ed
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-19 20:18:14 +0600

    delete doctor with confirmation

--- src/Pages/Dashboard/ManagedDoctors/ManagedDoctors.jsx
+++ src/Pages/Dashboard/ManagedDoctors/ManagedDoctors.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Image, Table } from 'react-bootstrap';
+import SweetAlert2 from 'react-sweetalert2';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 import Loading from '../../Shared/Loading/Loading';
 
 const ManagedDoctors = () => {
-    const { data: doctors = [], isLoading } = useQuery({
+    const [swalProps, setSwalProps] = useState({});
+    const { data: doctors = [], isLoading, refetch } = useQuery({
         queryKey: ['doctors'],
         queryFn: () => fetch(`http://localhost:5000/doctors`, {
             headers: {
@@ -13,6 +17,43 @@ const ManagedDoctors = () => {
         })
             .then(res => res.json())
     })
+
+    //# Delete doctor function
+    const MySwal = withReactContent(Swal)
+
+    const handleDeleteDoctor = (id) => {
+        MySwal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/doctor/${id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        authoraization: `bearer ${localStorage.getItem('doctor-portal')}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.acknowledged) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                            refetch()
+                        }
+                    })
+
+            }
+        })
+
+    }
 
     if (isLoading) {
         return <Loading></Loading>
@@ -43,7 +84,8 @@ const ManagedDoctors = () => {
                                 <td>{doc.name}</td>
                                 <td>{doc.email}</td>
                                 <td>{doc.speciality}</td>
-                                <td><Button variant='danger'>Delete</Button></td>
+                                <td><Button variant='danger' onClick={() => handleDeleteDoctor(doc._id)}>Delete</Button>
+                                </td>
                             </tr>
                         )}
                     </tbody>
