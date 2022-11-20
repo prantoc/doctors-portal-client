c48e3f8194a8ebaef0f6dfc105466ffa37bfca7f
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-20 18:20:25 +0600

    payment page and checkout form work for strip

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,15 @@
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-
+import { useLoaderData } from 'react-router-dom';
+const stripePromise = loadStripe(process.env.REACT_APP_Strip_Public);
+console.log(stripePromise);
 const Payment = () => {
+    const booking = useLoaderData();
+    const { _id, appointmentDate, slot, price, treatment } = booking;
     return (
         <div>
-            payment
+            <h3>Payment for <span className='text-primary fw-bold'>{treatment}</span></h3>
+            <p>Please pay <span className='fw-bold'>${price}</span> for your appointment on {appointmentDate} at {slot}</p>
         </div>
     );
 };
