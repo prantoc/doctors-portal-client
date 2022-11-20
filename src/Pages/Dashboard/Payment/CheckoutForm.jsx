f9ad5e83ecf4fc0fb6aca69cc9ad2c9167ce0dc5
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-20 21:43:11 +0600

    full payment system

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,10 +1,28 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { successToast } from '../../../toast/Toaster';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ booking }) => {
+    const { patientName, email, price } = booking;
     const [cardError, setCardError] = useState('')
+    const [clientSecret, setClientSecret] = useState("");
+    const [paymentData, setPaymentData] = useState();
     const stripe = useStripe();
     const elements = useElements();
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authoraization: `bearer ${localStorage.getItem('doctor-portal')}`
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -22,7 +40,7 @@ const CheckoutForm = () => {
             return;
         }
         // Use your card Element with other Stripe.js APIs
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
+        const { error } = await stripe.createPaymentMethod({
             type: 'card',
             card,
         });
@@ -31,36 +49,63 @@ const CheckoutForm = () => {
             // console.log('[error]', error);
             setCardError(error.message)
         } else {
-            setCardError()
-            console.log('[PaymentMethod]', paymentMethod);
+            setCardError('')
+            // console.log('[PaymentMethod]', paymentMethod);
         }
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: patientName,
+                        email: email
+                    },
+                },
+            },
+        );
 
+        if (confirmError) {
+            setCardError(confirmError.message)
+            return;
+        }
+        if (paymentIntent.status === "succeeded") {
+            successToast('Your Payment has been completed successfully!')
+            setPaymentData(paymentIntent.id)
+        }
+        console.log('Payment Intent', paymentIntent);
 
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            {cardError && <p className='text-danger fw-bold py-3'>{cardError}</p>}
-            <button className='btn btn-sm btn-primary mt-3 ' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                {cardError && <p className='text-danger fw-bold py-3'>{cardError}</p>}
+                <button className='btn btn-sm btn-primary mt-3 ' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+
+            {
+                paymentData && <p className='py-3'>Your Transaction id <span className='fw-bold text-dark'>#{paymentData}</span></p>
+            }
+        </>
     );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -3,8 +3,8 @@ import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import CheckoutForm from './CheckoutForm';
-const stripePromise = loadStripe(process.env.REACT_APP_Strip_PK);
-console.log(stripePromise);
+//#Strip API Key
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 const Payment = () => {
     const booking = useLoaderData();
     const { appointmentDate, slot, price, treatment } = booking;
@@ -14,7 +14,7 @@ const Payment = () => {
             <p>Please pay <span className='fw-bold'>${price}</span> for your appointment on {appointmentDate} at {slot}</p>
             <div className='shadow-lg p-3 my-3 bg-body rounded col-8'>
                 <Elements stripe={stripePromise}>
-                    <CheckoutForm />
+                    <CheckoutForm booking={booking} />
                 </Elements>
             </div>
         </div>
