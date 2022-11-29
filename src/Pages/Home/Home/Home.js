c0d0075586b222accfb4f43b242004efd6804f48
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-11-29 23:43:32 +0600

    use the title hook into home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import useTitle from '../../../hooks/useTitle';
 import Banner from '../Banner/Banner';
 import DentalCare from '../DentalCare/DentalCare';
 import InfoCards from '../InfoCards/InfoCards';
 import ServiceCards from '../ServiceCards/ServiceCards';
 const Home = () => {
+    useTitle('Home')
     return (
         <>
             <Banner></Banner>
